606f4b7eec1cc87040fa0512a94d454953af7d4e
j1mboj4mbo <themrarti@gmail.com>
2022-04-09 16:49:35 +0200

    09.04.2022

--- alpha/library/urls.py
+++ alpha/library/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     #aktualny czas
     path('time', views.timing, name='time'),
     #przeglądarka
-    path('browser', views.browser, name='browser')
+    path('browser', views.browser, name='browser'),
+    #książka
+    path('books/<int:book_id>/', views.book, name='book'),
+    #autor
+    path('author/<int:author_id>/', views.author, name='author')
 ]
--- alpha/library/views.py
+++ alpha/library/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import datetime
+from .models import Book, Author
 
 def index(request):
     """Strona główna"""
     text = ["sprawdzenie aktualnej pogody", "ładniejszy design strony"]
-    context = {"text": text}
+    books = Book.objects.all()
+    context = {"text": text, "books": books}
     return render(request, 'library/index.html', context)
 
 def timing(request):
@@ -14,11 +16,20 @@ def timing(request):
     return render(request, 'library/time.html', context)
 
 def browser(request):
-    browser = request.META['HTTP_USER_AGENT']
+    browser = request.headers['USER_AGENT'].split()[-1]
     context = {'browser': browser}
     return render(request, 'library/browser.html', context)
 
+def book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    description = book.description
+    author = book.author
+    context = {"book": book, "description": description, 'author': author}
+    return render(request, 'library/book.html', context)
 
-
-
+def author(request, author_id):
+    author = Author.objects.get(id=author_id)
+    books = author.book_set.all()
+    context = {'author': author, "books": books}
+    return render(request, 'library/author.html', context)
 
